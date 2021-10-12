17f571b572142c222e73933f8efb20e067227cd4
sophiegrafe <sophiegrafe.dev@gmail.com>
2021-10-12 16:54:31 +0200

    feature-search add logic to load results for query

--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,19 @@
-import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
 
+import { async } from 'regenerator-runtime';
+
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export async function loadRecipe(id) {
   try {
-    const data = await getJSON(`${API_URL}${id}`);
+    const data = await getJSON(`${API_URL}get?rId=${id}`);
 
     // reformating the obj we got from the server
     const { recipe } = data.data;
@@ -23,6 +28,25 @@ export async function loadRecipe(id) {
       ingredients: recipe.ingredients,
     });
   } catch (err) {
+    throw err;
+  }
+}
+
+export async function loadSearchResults(query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}search?q=${query}`);
+
+    state.search.results = data.recipes.map(recipe => {
+      return {
+        id: recipe.recipe_id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,
+      };
+    });
+  } catch (err) {
+    console.error(`${err.message}`);
     throw err;
   }
 }
