52aa1812206abb35e0be42333ca0dc1397f3da01
sophiegrafe <sophiegrafe.dev@gmail.com>
2021-10-12 16:50:21 +0200

    feature-search add searchCtrlr and sub for pub

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,28 +1,40 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+
+import { async } from 'regenerator-runtime';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
 ///////////////////////////////////////
 
 async function recipeController() {
   try {
-    recipeView.renderSpinner();
-
     const id = window.location.hash.slice(1);
     if (!id) return;
     // loading recipe
     await model.loadRecipe(id);
+    recipeView.renderSpinner();
     // rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.error(err.message, err.code);
     recipeView.renderError();
   }
 }
 
-function initSubscriber() {
-  recipeView.addHandlerRender(recipeController);
+async function searchController() {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+  } catch (err) {
+    recipeView.renderError();
+  }
 }
 
-initSubscriber();
+function initSubscribers() {
+  recipeView.addHandlerRender(recipeController);
+  searchView.addHandlerSearch(searchController);
+}
+
+initSubscribers();
