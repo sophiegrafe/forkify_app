2899d3356567b98b0fc62656ad435c764440420b
sophiegrafe <sophiegrafe.dev@gmail.com>
2021-10-13 17:52:03 +0200

    changed url args for getJSON to match v2 of API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,40 +1,48 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import { async } from 'regenerator-runtime';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 ///////////////////////////////////////
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
-async function recipeController() {
+async function recipesController() {
   try {
     const id = window.location.hash.slice(1);
     if (!id) return;
     // loading recipe
-    await model.loadRecipe(id);
     recipeView.renderSpinner();
+    await model.loadRecipe(id);
     // rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.log(err);
   }
 }
 
 async function searchController() {
   try {
+    resultsView.renderSpinner();
+
     const query = searchView.getQuery();
     if (!query) return;
 
     await model.loadSearchResults(query);
+
+    resultsView.render(model.state.search.results);
   } catch (err) {
     recipeView.renderError();
   }
 }
 
 function initSubscribers() {
-  recipeView.addHandlerRender(recipeController);
+  recipeView.addHandlerRender(recipesController);
   searchView.addHandlerSearch(searchController);
 }
-
 initSubscribers();
